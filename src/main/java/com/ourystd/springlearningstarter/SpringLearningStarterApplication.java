9c029163fa92c83302b1524d668cdff241fed847
Oury Diallo <amadoury313@gmail.com>
2025-07-21 05:27:39 +0000

    project bootstrap

---
+++ src/main/java/com/ourystd/springlearningstarter/SpringLearningStarterApplication.java
@@ -0,0 +1,13 @@
+package com.ourystd.springlearningstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ourystd/springlearningstarter/SpringLearningStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourystd.springlearningstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
