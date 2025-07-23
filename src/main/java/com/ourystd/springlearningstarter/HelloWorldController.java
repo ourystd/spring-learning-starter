c0a3f946c70fe1caa9d81f900621f87ba74fba79
Oury Diallo <amadoury313@gmail.com>
2025-07-23 04:05:03 +0000

    playing w/ env vars

--- src/main/java/com/ourystd/springlearningstarter/HelloWorldController.java
+++ src/main/java/com/ourystd/springlearningstarter/HelloWorldController.java
@@ -1,10 +1,26 @@
 package com.ourystd.springlearningstarter;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @Value("${SERVER_PORT}")
+    private String serverPort;
+    private final Environment env;
+
+    public HelloWorldController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping(path="/env")
+    public String env() {
+        return String.join(" ", this.env.getProperty("spring.application.name", "unknown"),
+                serverPort);
+    }
 
     @GetMapping(path="/hello")
     public String hello() {
