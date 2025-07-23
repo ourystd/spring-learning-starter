ecc9a71d8cadca30a6edfadde78365aede35dcee
Oury Diallo <amadoury313@gmail.com>
2025-07-23 05:00:57 +0000

    feat: connecting the app to h2 in memory database

--- src/main/java/com/ourystd/springlearningstarter/SpringLearningStarterApplication.java
+++ src/main/java/com/ourystd/springlearningstarter/SpringLearningStarterApplication.java
@@ -1,13 +1,31 @@
 package com.ourystd.springlearningstarter;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
 
 @SpringBootApplication
-public class SpringLearningStarterApplication {
+@Log
+public class SpringLearningStarterApplication implements CommandLineRunner {
+	private final DataSource dataSource;
+
+	public SpringLearningStarterApplication(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearningStarterApplication.class, args);
 	}
 
+	@Override
+	public void run(final String... args) {
+		log.info("DataSource: " + dataSource.toString());
+//		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+
+	}
 }
